3597349b9dca7622c7a9c4270394f014736f4ce6
Tuấn Anh <vtabk2@gmail.com>
2025-04-11 16:18:16 +0700

    Sửa lại NetworkUtils

--- app/src/main/java/com/example/gscore/MainActivity.kt
+++ app/src/main/java/com/example/gscore/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.gscore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.core.gscore.utils.network.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        lifecycleScope.launch(Dispatchers.IO) {
+            for (i in 0..100) {
+                Log.d("TAG5", "MainActivity_onCreate: i = $i")
+                NetworkUtils.hasInternetAccessCheck(
+                    doTask = {
+                        Log.d("TAG5", "MainActivity_onCreate: SUCCESS")
+                    }, doException = { networkError ->
+                        Log.d("TAG5", "MainActivity_onCreate: networkError = " + networkError.name)
+                    }, context = this@MainActivity, maxRetries = 3
+                )
+                delay(500)
+            }
         }
     }
 }
